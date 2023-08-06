8a6275dc807b05d83aa53b97f41a6c12f8573a63
Mike Thomsen <mikerthomsen@gmail.com>
2023-08-06 10:59:26 -0400

    Added project skeleton

---
+++ pojo-schema-test-api/src/main/java/org/apache/nifi/pojo/restapi/PojoSchemaTestApiApplication.java
@@ -0,0 +1,13 @@
+package org.apache.nifi.pojo.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojoSchemaTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojoSchemaTestApiApplication.class, args);
+	}
+
+}
